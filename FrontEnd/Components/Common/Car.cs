db912ca37bc315340e6678bb42276cdd0e3e9b89
Jack Kenney <jack@kenney.dev>
2024-10-11 15:13:22 -0400

    add some code

---
+++ FrontEnd/Components/Common/Car.cs
@@ -0,0 +1,20 @@
+public class Car
+{
+    public Car(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public static Task<Car[]> FetchCars()
+    {
+        // TODO: API call to backend
+        Car car = new Car(1, "Minivan");
+        Car[] cars = [car];
+
+        return Task.FromResult(cars);
+    }
+}
