c74631bb31a6f54416e5a4774c5914fba7114308
Jack Kenney <jack@kenney.dev>
2024-10-11 15:42:46 -0400

    blazor hybrid boilerplate

---
+++ FrontEnd/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace FrontEnd;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ FrontEnd/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace FrontEnd;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
