3f754e627b9aca910c7469b5afed0e82356083a0
Jack Kenney <jack@kenney.dev>
2024-10-12 10:20:44 -0400

    move common classes

---
+++ FrontEnd/Components/Common/User.cs
@@ -0,0 +1,22 @@
+namespace FrontEnd;
+
+public class User
+{
+    public User(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public static Task<Car[]> FetchUsers()
+    {
+        // TODO: API call to backend
+        User user = new User(1, "Jack");
+        User[] users = [user];
+
+        return Task.FromResult(users);
+    }
+}
